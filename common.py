3b7c04d40b342efc7bc62c51c3c4abeeb1e51bb3
David Fifield <david@bamsoftware.com>
2023-11-27 01:42:49 +0000

    Factor common code into common module.

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+import math
+
 KNOWN_SEDES = ("1", "2", "2.5", "3", "4", "4.5", "5", "6", "6.5", "7", "8", "8.5", "9", "10", "10.5", "11", "12")
 
 def is_metrically_permissible(shape, sedes):
@@ -12,3 +14,97 @@ def is_metrically_permissible(shape, sedes):
         if s > 24 or s == 23 or s % 4 not in (0, 2, 3):
             return False
     return True
+
+def is_metrically_permissible_anywhere(shape):
+    return any(is_metrically_permissible(shape, sedes) for sedes in map(float, KNOWN_SEDES))
+
+def shapes_gen_length(length):
+    if length <= 0:
+        yield ""
+    else:
+        for x in shapes_gen_length(length - 1):
+            yield "–" + x
+        for x in shapes_gen_length(length - 1):
+            yield "⏑" + x
+
+def shapes_gen():
+    i = 0
+    while True:
+        yield from shapes_gen_length(i)
+        i += 1
+
+# Colors for low-to-high color scale.
+COLOR_LOW  = (0x00, 0x00, 0x00)
+COLOR_HIGH = (0xe6, 0xe6, 0xe6)
+
+# Increase this to increase the steepness of the tone mapping operation.
+SHADE_MAPPING_ADJUST = 1.5
+
+# Text on a background of luminance less than this will be displayed as
+# light-on-dark rather than dark-on-light.
+LUMINANCE_INVERSION_THRESHOLD = 0.2
+
+def srgb_component_to_linear(s):
+    # https://en.wikipedia.org/wiki/SRGB#The_reverse_transformation
+    u = s / 255.0
+    if u <= 0.04045:
+        return u / 12.92
+    else:
+        return math.pow((u + 0.055)/1.055, 12.0/5.0)
+
+def linear_component_to_srgb(u):
+    # https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_(CIE_XYZ_to_sRGB)
+    if u <= 0.0031308:
+        s = 12.92 * u
+    else:
+        s = 1.055 * math.pow(u, 5.0/12.0) - 0.055
+    return int(s * 255 + 0.5)
+
+def srgb_to_linear(sr, sg, sb):
+    return tuple(srgb_component_to_linear(c) for c in (sr, sg, sb))
+
+def linear_to_srgb(r, g, b):
+    return tuple(linear_component_to_srgb(u) for u in (r, g, b))
+
+def interpolate_srgb(x, s1, s2):
+    return linear_to_srgb(*((1.0 - x) * u1 + x * u2 for (u1, u2) in zip(srgb_to_linear(*s1), srgb_to_linear(*s2))))
+
+def srgb_luminance(sr, sg, sb):
+    # https://en.wikipedia.org/wiki/Relative_luminance#Relative_luminance_in_colorimetric_spaces
+    r, g, b = srgb_to_linear(sr, sg, sb)
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+def tone_map(z):
+    # Logistic function.
+    return 1.0 / (1.0 + math.exp(-z * SHADE_MAPPING_ADJUST))
+
+def css_color(sr, sg, sb):
+    return f"#{sr:02x}{sg:02x}{sb:02x}"
+
+def z_css(z):
+    if z is None:
+        z = 0.0
+    srgb = interpolate_srgb(tone_map(z), COLOR_LOW, COLOR_HIGH)
+    return "; ".join((
+        "background-color:" + css_color(*srgb),
+        "color:" + "white" if srgb_luminance(*srgb) < LUMINANCE_INVERSION_THRESHOLD else "black",
+    ))
+
+# Return the mean of the sequence that arises from repeating each element e of
+# x, e times.
+def weighted_mean(x):
+    return sum(e*e for e in x) / sum(x)
+
+# Weighted population standard deviation, as in weighted_mean.
+def weighted_sd_pop(x):
+    μ = weighted_mean(x)
+    return math.sqrt(sum(e*(e - μ)**2 for e in x) / sum(x))
+
+def expectancy(x, xvec):
+    if sum(xvec) > 0:
+        μ = weighted_mean(xvec)
+        σ = weighted_sd_pop(xvec)
+        z = None if σ == 0.0 else (0 - μ) / σ
+    else:
+        z = None
+    return z
--- summary-table.py
+++ summary-table.py
@@ -3,97 +3,11 @@
 import collections
 import csv
 import html
-import math
 import sys
 
 import common
 
-# Colors for low-to-high color scale.
-COLOR_LOW  = (0x00, 0x00, 0x00)
-COLOR_HIGH = (0xe6, 0xe6, 0xe6)
-
-# Increase this to increase the steepness of the tone mapping operation.
-SHADE_MAPPING_ADJUST = 1.5
-
-# Text on a background of luminance less than this will be displayed as
-# light-on-dark rather than dark-on-light.
-LUMINANCE_INVERSION_THRESHOLD = 0.2
-
-def srgb_component_to_linear(s):
-    # https://en.wikipedia.org/wiki/SRGB#The_reverse_transformation
-    u = s / 255.0
-    if u <= 0.04045:
-        return u / 12.92
-    else:
-        return math.pow((u + 0.055)/1.055, 12.0/5.0)
-
-def linear_component_to_srgb(u):
-    # https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_(CIE_XYZ_to_sRGB)
-    if u <= 0.0031308:
-        s = 12.92 * u
-    else:
-        s = 1.055 * math.pow(u, 5.0/12.0) - 0.055
-    return int(s * 255 + 0.5)
-
-def srgb_to_linear(sr, sg, sb):
-    return tuple(srgb_component_to_linear(c) for c in (sr, sg, sb))
-
-def linear_to_srgb(r, g, b):
-    return tuple(linear_component_to_srgb(u) for u in (r, g, b))
-
-def interpolate_srgb(x, s1, s2):
-    return linear_to_srgb(*((1.0 - x) * u1 + x * u2 for (u1, u2) in zip(srgb_to_linear(*s1), srgb_to_linear(*s2))))
-
-def srgb_luminance(sr, sg, sb):
-    # https://en.wikipedia.org/wiki/Relative_luminance#Relative_luminance_in_colorimetric_spaces
-    r, g, b = srgb_to_linear(sr, sg, sb)
-    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-def tone_map(z):
-    # Logistic function.
-    return 1.0 / (1.0 + math.exp(-z * SHADE_MAPPING_ADJUST))
-
-def css_color(sr, sg, sb):
-    return f"#{sr:02x}{sg:02x}{sb:02x}"
-
-def z_css(z):
-    if z is None:
-        z = 0.0
-    srgb = interpolate_srgb(tone_map(z), COLOR_LOW, COLOR_HIGH)
-    return "; ".join((
-        "background-color:" + css_color(*srgb),
-        "color:" + "white" if srgb_luminance(*srgb) < LUMINANCE_INVERSION_THRESHOLD else "black",
-    ))
-
-# Return the mean of the sequence that arises from repeating each element e of
-# x, e times.
-def weighted_mean(x):
-    return sum(e*e for e in x) / sum(x)
-
-# Weighted population standard deviation, as in weighted_mean.
-def weighted_sd_pop(x):
-    μ = weighted_mean(x)
-    return math.sqrt(sum(e*(e - μ)**2 for e in x) / sum(x))
-
 Entry = collections.namedtuple("Entry", ("x", "z"))
-
-def shapes_gen_length(length):
-    if length <= 0:
-        yield ""
-    else:
-        for x in shapes_gen_length(length - 1):
-            yield "–" + x
-        for x in shapes_gen_length(length - 1):
-            yield "⏑" + x
-
-def shapes_gen():
-    i = 0
-    while True:
-        yield from shapes_gen_length(i)
-        i += 1
-
-def is_metrically_permissible_anywhere(shape):
-    return any(common.is_metrically_permissible(shape, sedes) for sedes in map(float, common.KNOWN_SEDES))
 
 M = {}
 r = csv.DictReader(sys.stdin)
@@ -149,10 +63,10 @@ for sedes in common.KNOWN_SEDES:
 print("<th>Total</th>")
 print("</tr>")
 
-for shape in shapes_gen():
+for shape in common.shapes_gen():
     if len(shape) > 8:
         break
-    if not is_metrically_permissible_anywhere(shape):
+    if not common.is_metrically_permissible_anywhere(shape):
         print("<!--")
         for sedes in map(float, common.KNOWN_SEDES):
             assert M.get((shape, sedes)) is None, (shape, sedes)
@@ -170,13 +84,7 @@ for shape in shapes_gen():
             print("<td class=impermissible>✖</td>")
         else:
             if entry is None:
-                if sum(xvec) > 0:
-                    μ = weighted_mean(xvec)
-                    σ = weighted_sd_pop(xvec)
-                    z = None if σ == 0.0 else (0 - μ) / σ
-                else:
-                    z = None
-                entry = Entry(0, z)
+                entry = Entry(0, common.expectancy(0, xvec))
             try:
                 del M[(shape, sedes)]
             except KeyError:
@@ -184,10 +92,10 @@ for shape in shapes_gen():
             contents = "<span class=x>" + html.escape("{:,}".format(entry.x)) + "</span>"
             if entry.z is not None:
                 contents += "<br><span class=z>" + html.escape("{:+.03f}".format(entry.z).replace("-", "−")) + "</span>"
-            print(f"<td style=\"{html.escape(z_css(entry.z))}\">{contents}</td>")
+            print(f"<td style=\"{html.escape(common.z_css(entry.z))}\">{contents}</td>")
     print(f"<td><span class=x>{html.escape('{:,}'.format(sum(xvec)))}</span></td>")
     print("</tr>")
-    if not is_metrically_permissible_anywhere(shape):
+    if not common.is_metrically_permissible_anywhere(shape):
         print("-->")
 assert len(M) == 0, M
 
