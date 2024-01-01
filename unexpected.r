0a9813ee09f31eb85f2e7bf11771d5ca7c8948e0
David Fifield <david@bamsoftware.com>
2024-01-01 21:14:41 +0000

    Add era to unexpected.r.

--- unexpected.r
+++ unexpected.r
@@ -10,7 +10,16 @@ data <- read_csv("joined.sedes-metrical_shape.csv", col_types = cols(
 	z = col_double(),
 )) %>% mutate(
 	is_unexpected = z <= Z_THRESHOLD,
+	era = fct(case_when(
+		work %in% c("Il.", "Od.", "Hom.Hymn", "Theog.", "W.D.", "Sh.") ~ "archaic",
+		work %in% c("Argon.", "Callim.Hymn", "Phaen.", "Theoc.") ~ "hellenistic",
+		work %in% c("Q.S.", "Dion.") ~ "imperial",
+	), levels = c("archaic", "hellenistic", "imperial")),
 )
+
+if (any(is.na(data$era))) {
+	stop()
+}
 
 cat(sprintf("z threshold is <= %+.2f\n", Z_THRESHOLD))
 
@@ -30,10 +39,10 @@ num_unexpected_iliad <- nrow(filter(iliad, is_unexpected))
 cat(sprintf("Percentage of unexpected words %d/%d = %.2f%%\n", num_unexpected_iliad, num_words_iliad, num_unexpected_iliad / num_words_iliad * 100))
 cat(sprintf("Unexpected words per book %d/%d = %.2f\n", num_unexpected_iliad, 24, num_unexpected_iliad / 24))
 cat(sprintf("Average words per book %d/%d = %.2f\n", num_words_iliad, 24, num_words_iliad / 24))
-print(iliad %>% group_by(book_n) %>% summarize(num_words = n(), num_unexpected = sum(is_unexpected, na.rm = TRUE), pct_unexpected = num_unexpected / num_words * 100) %>% arrange(pct_unexpected), n = 100)
+print(iliad %>% group_by(book_n) %>% summarize(num_words = n(), num_unexpected = sum(is_unexpected, na.rm = TRUE), pct_unexpected = num_unexpected / num_words * 100, era = first(era)) %>% arrange(pct_unexpected), n = 100)
 
 cat("\n")
 
-cat("Longer poems\n")
+cat("Longer poems, by book\n")
 
-print(data %>% group_by(work, book_n) %>% summarize(num_words = n(), num_unexpected = sum(is_unexpected, na.rm = TRUE), pct_unexpected = num_unexpected / num_words * 100, .groups = "drop") %>% filter(num_words > 1000) %>% arrange(pct_unexpected), n = 500)
+print(data %>% group_by(work, book_n) %>% summarize(num_words = n(), num_unexpected = sum(is_unexpected, na.rm = TRUE), pct_unexpected = num_unexpected / num_words * 100, era = first(era), .groups = "drop") %>% filter(num_words > 1000) %>% arrange(pct_unexpected), n = 500)
