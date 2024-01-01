813d921afdea38e697fe96082e267388e566d9ff
David Fifield <david@bamsoftware.com>
2024-01-01 21:16:19 +0000

    Unexpected per windows of 50 words.

--- unexpected.r
+++ unexpected.r
@@ -1,4 +1,5 @@
 library("tidyverse")
+library("RcppRoll")
 
 Z_THRESHOLD <- -2.0
 
@@ -9,7 +10,7 @@ data <- read_csv("joined.sedes-metrical_shape.csv", col_types = cols(
 	x = col_double(),
 	z = col_double(),
 )) %>% mutate(
-	is_unexpected = z <= Z_THRESHOLD,
+	is_unexpected = !is.na(z) & z <= Z_THRESHOLD,
 	era = fct(case_when(
 		work %in% c("Il.", "Od.", "Hom.Hymn", "Theog.", "W.D.", "Sh.") ~ "archaic",
 		work %in% c("Argon.", "Callim.Hymn", "Phaen.", "Theoc.") ~ "hellenistic",
@@ -58,3 +59,39 @@ cat("\n")
 cat("By era\n")
 
 print(data %>% group_by(era) %>% summarize(num_words = n(), num_unexpected = sum(is_unexpected, na.rm = TRUE), pct_unexpected = num_unexpected / num_words * 100, era = first(era), .groups = "drop") %>% filter(num_words > 1000) %>% arrange(pct_unexpected), n = 500)
+
+cat("\n")
+
+cat("Unexpected shapes per window of 50 words, archaic corpus\n")
+
+data <- data %>%
+	group_by(work, book_n) %>%
+	mutate(unexpected_50 = roll_sum(is_unexpected, 50, align = "left", fill = NA)) %>%
+	ungroup()
+
+archaic_data <- filter(data, era == "archaic")
+summary(archaic_data$unexpected_50)
+x <- table(archaic_data$unexpected_50)
+x
+x / sum(x) * 100
+cumsum(x) / sum(x) * 100
+p <- ggplot(archaic_data) +
+	geom_bar(aes(unexpected_50)) +
+	scale_x_continuous(breaks = 0:10) +
+	scale_y_continuous(minor_breaks = waiver()) +
+	labs(
+		x = "Number of unexpected shapes in window",
+		y = "Number of distinct windows",
+		title = bquote("Unexpected shapes (" * italic(z) ~ "≤" ~ .(gsub("-", "−", sprintf("%-.1f", Z_THRESHOLD))) * ") per window of 50 lines, Archaic corpus")
+	)
+ggsave("unexpected-window-50.archaic.png", p, width = 7.5, height = 3, dpi = 200)
+p <- ggplot(archaic_data) +
+	stat_ecdf(aes(unexpected_50), geom = "bar") +
+	scale_x_continuous(breaks = 0:10) +
+	scale_y_continuous(n.breaks = 11, labels = scales::percent, minor_breaks = NULL) +
+	labs(
+		x = "Number of unexpected shapes in window",
+		y = "Number of distinct windows",
+		title = bquote("Unexpected shapes (" * italic(z) ~ "≤" ~ .(gsub("-", "−", sprintf("%-.1f", Z_THRESHOLD))) * ") per window of 50 lines, cumulative, Archaic corpus")
+	)
+ggsave("unexpected-window-50-cumul.archaic.png", p, width = 7.5, height = 3, dpi = 200)
