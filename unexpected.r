82fa662d93a2c38e2b971b4b88b989ba0f248965
David Fifield <david@bamsoftware.com>
2024-06-05 22:40:21 +0000

    Try geom_step in place of geom_col.

--- unexpected.r
+++ unexpected.r
@@ -126,10 +126,10 @@ unique_line_n <- function(line_n) {
 begin_unique_line_n <- unique_line_n("409b")
 end_unique_line_n <- unique_line_n("442")
 p <- ggplot() +
-	geom_col(data = data_hom_hymn_4, aes(
+	geom_step(data = data_hom_hymn_4, aes(
 		x = unique_word_n,
 		y = unexpected_window,
-	), width = 2.0) +
+	), linewidth = 0.15) +
 	geom_point(data = filter(data_hom_hymn_4, is_unexpected), aes(
 		x = unique_word_n,
 		y = -0.25,
